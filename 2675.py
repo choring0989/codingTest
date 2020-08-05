29f30379b387dd195c2a109fbd49a17d9356fc90
JiAn Lee <32667619+choring0989@users.noreply.github.com>
2020-08-05 17:05:11 +0900

    Update 2675.py
    수정!

--- 2675.py
+++ 2675.py
@@ -1,6 +1,14 @@
+"""
 for i in range(int(input())):
     redo, str= input().split(" ")
     for j in str:
         for k in range(int(redo)):
             print(j, end="")
+    print()
+"""
+
+for i in range(int(input())):
+    redo, str = input().split(" ")
+    for j in str:
+        print(int(redo)*j, end="")
     print()
