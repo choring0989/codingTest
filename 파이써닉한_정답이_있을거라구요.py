c8bedd14b71cec0708216c5eda770fdfccaa5907
JiAn Lee <32667619+choring0989@users.noreply.github.com>
2020-08-07 05:45:14 +0900

    Update 파이써닉한_정답이_있을거라구요.py

--- 파이써닉한_정답이_있을거라구요.py
+++ 파이써닉한_정답이_있을거라구요.py
@@ -1,5 +1,10 @@
+'''
+#이렇게 짜면 안 돼요
 def solution(mylist):
     answer = list()
     for j in mylist:
         answer.append(len(j))
     return answer
+'''
+def solution(mylist):
+    return list(map(len, mylist))
